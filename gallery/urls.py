a8822d89aca0dea638160f28b4930e4362085402
Kamil <kubiak.kamil23@gmail.com>
2024-02-21 01:18:20 +0100

    add merge for pull

--- gallery/urls.py
+++ gallery/urls.py
@@ -3,10 +3,6 @@ from gallery.views import GalleryListView
 
 app_name = 'gallery'
 urlpatterns = [
-<<<<<<< HEAD
     path('', GalleryListView.as_view(), name='galleryList')
-=======
-    path('', GalleryListView.as_view(), name='galleryList'),
-
->>>>>>> 17099ceb532e315c9c8d4063c785f67955ad4583
 ]
+
--- gallery/views.py
+++ gallery/views.py
@@ -2,17 +2,6 @@ import logging
 from django.contrib.auth.mixins import LoginRequiredMixin
 from django.shortcuts import render
 from django.views import View
-<<<<<<< HEAD
-
-from main.views import MainModule, GroupPermission
-
-logger = logging.getLogger(__name__)
-
-
-# Create your views here.
-class GalleryListView(LoginRequiredMixin, View):
-    template = "gallery/list_gallery.html"
-=======
 
 from main.views import MainModule, GroupPermission
 
@@ -22,7 +11,6 @@ logger = logging.getLogger(__name__)
 class GalleryListView(LoginRequiredMixin, View):
     method = "GalleryListView"
     template_name = "gallery/list_gallery.html"
->>>>>>> 17099ceb532e315c9c8d4063c785f67955ad4583
     template_error = 'main/error_site.html'
 
     def get(self, request):
@@ -33,7 +21,6 @@ class GalleryListView(LoginRequiredMixin, View):
         donation_group = request.user.groups.filter(name=GroupPermission.donation.value).exists()
         gallery_group = request.user.groups.filter(name=GroupPermission.gallery.value).exists()
         try:
-<<<<<<< HEAD
             # galleries = Gallery.objects.all()
             # gallery_count = len(galleries)
             # context = {'galleries': galleries,
@@ -43,17 +30,8 @@ class GalleryListView(LoginRequiredMixin, View):
                        'project_group': project_group, 'plan_group': plan_group, 'donation_group': donation_group,
                        'gallery_group': gallery_group}
 
-            return render(request, self.template, context)
+            return render(request, self.template_name, context)
         except Exception as e:
             logger.error("Error: %s", e)
             context = {'error_message': f"Wystąpił błąd {e}"}
-=======
-
-            context = {'title': MainModule.gallery.value, 'unit_group': unit_group, 'contract_group': contract_group,
-                       'project_group': project_group, 'plan_group': plan_group, 'donation_group': donation_group,
-                       'gallery_group': gallery_group}
-            return render(request, self.template_name, context)
-        except Exception as e:
-            context = {'error_message': e, 'method': self.method}
->>>>>>> 17099ceb532e315c9c8d4063c785f67955ad4583
             return render(request, self.template_error, context)
